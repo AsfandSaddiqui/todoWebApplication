82a1f9e61731cb9187556e18e957415e5eba44e4
Asfand Saddiqui <78793872+AsfandSaddiqui@users.noreply.github.com>
2021-08-22 17:25:18 +0500

    Fix Local Storage Problem | Code Refactoring

--- src/components/addTodo/__test__/AddTodo.test.js
+++ src/components/addTodo/__test__/AddTodo.test.js
@@ -1,29 +1,9 @@
-import { render, screen, fireEvent, cleanup } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import AddTodo from "../AddTodo";
 
-beforeEach(() => {
-  render(<AddTodo />);
-});
-
-/**
- * Helper Methods
- * These methods get the most frequntly used elements for testing
- * */
-
-const addTask = (task) => {
-  const inputEl = screen.getByPlaceholderText("Add your new todo");
-  const btnEl = screen.getByTestId("btn");
-
-  task.forEach((el) => {
-    fireEvent.change(inputEl, { target: { value: el } });
-    fireEvent.click(btnEl);
-  });
-};
-
-const removeTask = (task) => {
-  task.forEach((tsk) => {
-    fireEvent.click(tsk);
-  });
+const cleanLocalStorage = () => {
+  localStorage.setItem("list", JSON.stringify([]));
 };
 
 /**
@@ -33,31 +13,27 @@ const removeTask = (task) => {
 
 describe("Unit Testing of Component", () => {
   it("should be empty by deafult", () => {
-    const inputEl = screen.getByTestId("input");
+    const { getByPlaceholderText } = render(<AddTodo />);
+    const inputEl = getByPlaceholderText("Add your new todo");
     expect(inputEl.value).toBe("");
   });
 
   it("should change input value", () => {
-    const inputEl = screen.getByTestId("input");
-    fireEvent.change(inputEl, {
-      target: {
-        value: "hello i am test",
-      },
-    });
+    const { getByPlaceholderText } = render(<AddTodo />);
+    const inputEl = getByPlaceholderText("Add your new todo");
+    userEvent.type(inputEl, "hello i am test");
     expect(inputEl.value).toBe("hello i am test");
   });
 
   it("should empty input field after each click", () => {
-    const inputEl = screen.getByTestId("input");
-    const btnEl = screen.getByTestId("btn");
-    fireEvent.change(inputEl, {
-      target: {
-        value: "hello test1 ",
-      },
-    });
+    const { getByTestId, getByPlaceholderText } = render(<AddTodo />);
+    const inputEl = getByPlaceholderText("Add your new todo");
+    const btnEl = getByTestId("add_btn");
+    userEvent.type(inputEl, "hello test1 ");
 
-    fireEvent.click(btnEl);
+    userEvent.click(btnEl);
     expect(inputEl.value).toBe("");
+    cleanLocalStorage();
   });
 });
 
@@ -66,36 +42,62 @@ describe("Unit Testing of Component", () => {
  * Test 2 components 'Todo' and 'AddTodo'
  **/
 
-describe("Integration Testing of Component", () => {
-  afterEach(() => {
-    cleanup();
-  });
-
+describe("Integration Testing of components", () => {
   it("should add value to todo List", () => {
-    const inputEl = screen.getByPlaceholderText("Add your new todo");
-    const btnEl = screen.getByTestId("btn");
-    fireEvent.change(inputEl, {
-      target: {
-        value: "hello test1",
-      },
-    });
+    const { getByPlaceholderText, getByTestId, getByText } = render(
+      <AddTodo />
+    );
+    const inputEl = getByPlaceholderText("Add your new todo");
+    const btnEl = getByTestId("add_btn");
+    userEvent.type(inputEl, "hello test");
 
-    fireEvent.click(btnEl);
-    const listEl = screen.getByText(/hello test1/i);
+    userEvent.click(btnEl);
+    const listEl = getByText(/hello test/i);
+
     expect(listEl).toBeInTheDocument;
+    cleanLocalStorage();
   });
 
   it("should add multiple value to todo List", () => {
-    addTask(["test 1", "test 2", "test 3", "test 4", "test 5"]);
-    const listEl = screen.getAllByTestId("btn2");
+    const { getAllByTestId, getByPlaceholderText, getByTestId } = render(
+      <AddTodo />
+    );
+    const task = ["test 1", "test 2", "test 3", "test 4", "test 5"];
+
+    const inputEl = getByPlaceholderText("Add your new todo");
+    const btnEl = getByTestId("add_btn");
+
+    task.forEach((el) => {
+      userEvent.type(inputEl, el);
+      userEvent.click(btnEl);
+    });
+
+    const listEl = getAllByTestId("icon");
     expect(listEl.length).toBe(5);
+    cleanLocalStorage();
   });
 
-  it("should remove when click on check button", () => {
-    const listEl = screen.getAllByTestId("btn2");
+  it("should remove multiple todo when click on check button", () => {
+    const { getAllByTestId, getByPlaceholderText, getByTestId } = render(
+      <AddTodo />
+    );
 
-    removeTask(listEl);
+    const task = ["test 1", "test 2", "test 3", "test 4", "test 5"];
 
-    expect(listEl.length).toBe(0);
+    const inputEl = getByPlaceholderText("Add your new todo");
+    const btnEl = getByTestId("add_btn");
+
+    task.forEach((task) => {
+      userEvent.type(inputEl, task);
+      userEvent.click(btnEl);
+    });
+    const todos = getAllByTestId("icon");
+    expect(todos.length).toBe(5);
+
+    todos.forEach((todo) => {
+      userEvent.click(todo);
+    });
+
+    expect(todos).not.toBeInTheDocument;
   });
 });
